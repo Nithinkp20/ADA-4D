352b72ad796a34ab36c5fb0444c572e1ed856da2
Nithinkp20 <153255392+Nithinkp20@users.noreply.github.com>
2024-07-18 05:19:43 +0530

    Update Johnson_Trotter.c

--- LAB7/Johnson_Trotter.c
+++ LAB7/Johnson_Trotter.c
@@ -14,41 +14,51 @@ void main(){
     printf("Enter sorted elements: ");
     for(int i = 0; i < n; i++){
         scanf("%d", &e[i].num);
-        e[i].dir = 1;
+        e[i].dir = -1;  // -1 indicates left, 1 indicates right
     }
     while(1){
         for(int i = 0; i < n; i++){
             printf("%d ", e[i].num);
         }
         printf("\n");
+
         int curri = -1;
+        // Find the largest mobile integer
         for(int i = 0; i < n; i++){
-            if(curri == -1 || e[i].num > e[curri].num){
-                if(e[i].dir == 1){
-                    if(i < n - 1 && e[i+1].num < e[i].num){
-                        curri = i;
-                    }
+            if(e[i].dir == 1 && i < n - 1 && e[i].num > e[i + 1].num){
+                if(curri == -1 || e[i].num > e[curri].num){
+                    curri = i;
                 }
-                else{
-                    if(i > 0 && e[i].num > e[i-1].num){
-                        curri = i;
-                    }
+            }
+            if(e[i].dir == -1 && i > 0 && e[i].num > e[i - 1].num){
+                if(curri == -1 || e[i].num > e[curri].num){
+                    curri = i;
                 }
             }
         }
+
+        // If there is no mobile integer, break the loop
         if(curri == -1) break;
-        struct ele temp = e[curri];
+
+        // Swap the largest mobile integer
+        struct ele temp;
         if(e[curri].dir == 1){
+            temp = e[curri];
             e[curri] = e[curri + 1];
             e[curri + 1] = temp;
-        }
-        else{
+            curri++;
+        } else {
+            temp = e[curri];
             e[curri] = e[curri - 1];
             e[curri - 1] = temp;
+            curri--;
         }
+
+        // Reverse the direction of all elements larger than the current largest mobile integer
         for(int i = 0; i < n; i++){
             if(e[i].num > temp.num){
-                e[i].dir = !e[i].dir;
+                e[i].dir = -e[i].dir;
             }
         }
     }
+}
